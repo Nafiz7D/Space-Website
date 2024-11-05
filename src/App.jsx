07d9fe13349cf984a7157f8bccd62abb026fc418
Nafiz7D <mrnafsu07@gmail.com>
2024-11-05 10:23:14 +0600

    added hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import bgVideo from "./assets/earth-bg.mp4";
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
     <div>
       <div className="h-[700px] relative">
         <video autoPlay loop muted
-        className="fixed right-0 top-0 h-[700[px] w-full object-cover z-[-1]"
+        className="fixed right-0 top-0 h-[700px] w-full object-cover"
         src={bgVideo}></video>
         <Navbar></Navbar>
+        <Hero></Hero>
       </div>
+
+      {/* services Card Section */}
+      <Services></Services>
     </div>
   );
 };
